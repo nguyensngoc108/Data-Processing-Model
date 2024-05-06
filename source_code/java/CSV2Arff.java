3acf250eaf60bec58e3f67d71162ad7aa5be0add
DominicDang <90438687+DominicDang@users.noreply.github.com>
2024-05-06 17:48:04 +0700

    Data Preprocessing with Weka API in Java

---
+++ source_code/java/CSV2Arff.java
@@ -0,0 +1,25 @@
+package weka.api;
+
+//import required classes
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import java.io.File;
+
+public class CSV2Arff {
+	public static void main(String[] args) throws Exception {
+		
+		// Load CSV
+		CSVLoader loader = new CSVLoader();
+		loader.setSource(new File("D:\\Year 4\\Data Mining\\data\\sales_clothes.csv"));
+		Instances data = loader.getDataSet(); //get instances object
+		
+		// Save ARFF
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(data); //set the dataset we want to convert
+		
+		// Save as ARFF file
+		saver.setFile(new File("D:\\Year 4\\Data Mining\\data\\sales_clothes.arff"));
+		saver.writeBatch();
+	}
+}
