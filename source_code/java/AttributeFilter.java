471c44e4ef7c91f90ae6127dbc3e2a16329c4f8a
DominicDang <90438687+DominicDang@users.noreply.github.com>
2024-05-07 21:24:11 +0700

    Update AttributeFilter.java

--- source_code/java/AttributeFilter.java
+++ source_code/java/AttributeFilter.java
@@ -14,25 +14,14 @@ public class AttributeFilter {
         // Load data
         DataSource source = new DataSource("D:\\Year 4\\Data Mining\\data\\sales_clothes.arff");
         Instances dataset = source.getDataSet();
-        
-        // Create a RenameAttribute filter
-        String[] str = new String[]{"-R", "first", "-replace", "product_id"};
-        // Create a Remove object (this is the filter class)
-        RenameAttribute rename = new RenameAttribute();
-        // Set filter options
-        rename.setOptions(str);
-        // Pass the data to apply filter
-        rename.setInputFormat(dataset);
-        Instances renamedData = Filter.useFilter(dataset, rename);
-        
 
         // Specify the attributes to convert
         int[] attributesToConvert = {3, 4, 13, 14, 15, 19}; // Change the attribute indices as needed
         // Apply the NumericToNominal filter only to specified attributes
         NumericToNominal numericToNominalFilter = new NumericToNominal();
         numericToNominalFilter.setAttributeIndicesArray(attributesToConvert);
-        numericToNominalFilter.setInputFormat(renamedData);
-        Instances convertedData = Filter.useFilter(renamedData, numericToNominalFilter);
+        numericToNominalFilter.setInputFormat(dataset);
+        Instances convertedData = Filter.useFilter(dataset, numericToNominalFilter);
 
         // Remove unnecessary attributes
         String[] opts = new String[]{"-R", "1, 21, 22, 23"};
